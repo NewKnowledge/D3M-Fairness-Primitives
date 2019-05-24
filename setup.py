2458a2e02651c1fc2bd591d0b959ebe1eddaeefd
Jeffrey Gleason <jeffrey.gleason@newknowledge.io>
2019-05-24 15:57:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-setup(name='D3MFairnessPrimitives',
+setup(name='FairnessPrimitives',
     version='1.0.0',
     description='Pre-processing, in-processing, and post-processing algorithms wrapped for the D3M infrastructure',
-    packages=['D3MFairnessPrimitives'],
+    packages=['FairnessPrimitives'],
     install_requires=["aif360",
                       ],
     entry_points = {
         'd3m.primitives': [
-            'data_preprocessing.data_conversion.FairnessPreProcessing = D3MFairnessPrimitives:pre_processing',
-            'data_augmentation.data_conversion.FairnessInProcessing = D3MFairnessPrimitives:in_processing',
-            'data_augmentation.data_conversion.FairnessPostProcessing = D3MFairnessPrimitives:post_processing',
+            'data_preprocessing.data_conversion.FairnessPreProcessing = FairnessPrimitives:pre_processing',
+            'data_augmentation.data_conversion.FairnessInProcessing = FairnessPrimitives:in_processing',
+            'data_augmentation.data_conversion.FairnessPostProcessing = FairnessPrimitives:post_processing',
        ],
     },
 )
