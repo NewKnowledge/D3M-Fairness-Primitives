d6e3dfd09fc4fcc752cfd5496506f458d183e858
jgleason <jeffrey.gleason@newknowledge.io>
2019-05-24 16:04:32 -0500

    update setup and pipeline

--- FairnessPrimitives/pre_processing_pipeline_uu5_heartstatlog.py
+++ FairnessPrimitives/pre_processing_pipeline_uu5_heartstatlog.py
@@ -3,7 +3,6 @@ from d3m.metadata.base import ArgumentType, Context
 from d3m.metadata.pipeline import Pipeline, PrimitiveStep
 
 # Creating pipeline
-pipeline_description = Pipeline(context=Context.TESTING)
 pipeline_description.add_input(name='inputs')
 
 # Step 0: Denormalize primitive
