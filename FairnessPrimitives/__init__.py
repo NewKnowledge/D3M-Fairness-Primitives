e45c26669935731c5da5bd437468ee159187e707
jgleason <jeffrey.gleason@newknowledge.io>
2019-05-24 17:40:37 -0500

    add init file

---
+++ FairnessPrimitives/__init__.py
@@ -0,0 +1,11 @@
+from FairnessPrimitives.FairnessPreProcessing import FairnessPreProcessing
+#from FairnessPrimitives.FairnessInProcessing import FairnessInProcessing
+#from FairnessPrimitives.FairnessPostProcessing import FairnessPostProcessing
+
+__version__ = '1.0.0'
+
+__all__ = [
+           "FairnessPreProcessing", 
+           #"FairnessInProcessing", 
+           #"FairnessPostProcessing",
+           ]
