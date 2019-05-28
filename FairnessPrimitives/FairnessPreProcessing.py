8d27738330918ef5009f2a6f84232d76573d7dc7
jgleason <jeffrey.gleason@newknowledge.io>
2019-05-28 11:02:05 -0500

    BasePrimitive instead of Transformer

--- FairnessPrimitives/FairnessPreProcessing.py
+++ FairnessPrimitives/FairnessPreProcessing.py
@@ -7,7 +7,7 @@ from typing import List
 
 from Sloth import cluster
 
-from d3m.primitive_interfaces.transformer import TransformerPrimitiveBase
+from d3m.primitive_interfaces.transformer import PrimitiveBase
 from d3m.primitive_interfaces.base import CallResult
 
 from d3m import container, utils, exceptions
@@ -16,6 +16,8 @@ from d3m.metadata import hyperparams, base as metadata_base, params
 from common_primitives import utils as utils_cp, dataset_to_dataframe as DatasetToDataFrame
 
 from aif360 import datasets, algorithms
+from aif360.algorithms import preprocessing
+from aif360.algorithms.preprocessing import optim_preproc_helpers
 
 __author__ = 'Distil'
 __version__ = '1.0.0'
@@ -41,10 +43,10 @@ class Hyperparams(hyperparams.Hyperparams):
             default=(),
             semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
             description="One subset of protected attribute values which are considered privileged from a fairness perspective",
-        )
+        ),
         default=[],
         semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
-        description= 'A list of subsets of protected attribute values which are considered privileged from a fairness perspective",
+        description= "A list of subsets of protected attribute values which are considered privileged from a fairness perspective",
     )
     favorable_label = hyperparams.Bounded[float](
         lower=0.,
@@ -55,9 +57,13 @@ class Hyperparams(hyperparams.Hyperparams):
     )
     pass
 
-class FairnessPreProcessing(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
+class FairnessPreProcessing(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     '''
-        
+        Primitive that applies one of four pre-processing algorithm to training data before fitting step of pipeline. Algorithm
+        options are 'Disparate_Impact_Remover', 'Learning_Fair_Representations', 'Optimized_Preprocessing', and 'Reweighing'.
+
+        Fit method applies primitive pre-processing. Produce method leaves dataset as is. 
+
     '''
     metadata = metadata_base.PrimitiveMetadata({
         # Simply an UUID generated once and fixed forever. Generated using "uuid.uuid4()".
@@ -90,7 +96,7 @@ class FairnessPreProcessing(TransformerPrimitiveBase[Inputs, Outputs, Hyperparam
         # Choose these from a controlled vocabulary in the schema. If anything is missing which would
         # best describe the primitive, make a merge request.
         'algorithm_types': [
-            metadata_base.PrimitiveAlgorithmType.,
+            metadata_base.PrimitiveAlgorithmType.DATA_CONVERSION,
         ],
         'primitive_family': metadata_base.PrimitiveFamily.DATA_PREPROCESSING,
     })
@@ -98,38 +104,62 @@ class FairnessPreProcessing(TransformerPrimitiveBase[Inputs, Outputs, Hyperparam
     def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0)-> None:
         super().__init__(hyperparams=hyperparams, random_seed=random_seed)
 
+        self.inputs = None
+        self.targets = None
 
-    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
-        """
+    def get_params(self) -> Params:
+        return self._params
+
+    def set_params(self, *, params:Params) -> None:
+        self.params = params
+
+    def set_training_data(self, *, inputs: Inputs, outputs: Outputs) -> None:
+        '''
+        Sets primitive's training data
         Parameters
         ----------
-        inputs : D3M dataset
+        inputs: d3m dataframe
+        outputs: d3m dataframe
+        '''
 
-        Returns
-        ----------
-        Outputs : D3M dataset with a new number of edited records after one of four fairness pre-processing algorithms 
-            have been applied to the dataset
-            
-        """
-
-        # confirm that length of protected_attribute_cols HP and privileged_protected_attributes HP are the same
-        if len(self.hyperparams['protected_attribute_cols']) != len(self.hyperparams['privileged_protected_attributes'])
-
-        # transfrom dataframe to IBM 360 compliant dataset
-            # 1. assume datacleaning primitive has been applied so there are no NAs
-            # 2. assume PandasOneHotEncoderPrimitive has also been applied to categorical columns
+        # only select attributes from training data
+        attributes = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/Attribute')
+        attribute_names = [list(inputs)[a] for a in attributes]
+        self.inputs = inputs[[attribute_names]]
         targets = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/TrueTarget')
         if not len(targets):
             targets = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/TrueTarget')  
         if not len(targets):
             targets = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/SuggestedTarget')
-        label_names = list(inputs)[targets]
-        protected_attributes = list(inputs)[self.hyperparams['protected_attribute_cols']]
+        self.targets = targets
+
+    def fit(self, *, timeout: float = None, iterations: int = None) -> CallResult[None]:
+        """
+        Parameters
+        ----------
+        inputs : D3M dataframe
+
+        Returns
+        ----------
+        Outputs : D3M dataframe with a new number of edited records after one of four fairness pre-processing algorithms 
+            have been applied to the dataset
+        """
+
+        # confirm that length of protected_attribute_cols HP and privileged_protected_attributes HP are the same
+        if len(self.hyperparams['protected_attribute_cols']) != len(self.hyperparams['privileged_protected_attributes'])
+            raise exceptions.InvalidArgumentValueError("The number of protected attributes and the number of lists of privileged values for these + \
+                                                       protected attributes must be the same")
+        
+        # transfrom dataframe to IBM 360 compliant dataset
+            # 1. assume datacleaning primitive has been applied so there are no NAs
+            # 2. assume PandasOneHotEncoderPrimitive has also been applied to categorical columns
+        label_names = [list(self.inputs)[target] for target in self.targets]
+        protected_attributes = [list(self.inputs)[c] for c in self.hyperparams['protected_attribute_cols']]
         unfavorable_label = 0. if self.hyperparams['favorable_label'] == 1. else 1.
-        ibm_dataset = datasets.BinaryLabelDataset(inputs, 
+        ibm_dataset = datasets.BinaryLabelDataset(df = self.inputs, 
                                                 label_names = label_names, 
                                                 protected_attribute_names = protected_attributes, 
-                                                privileged_protected_attributes = self.hyperparams['privileged_protected_attributes']
+                                                privileged_protected_attributes = self.hyperparams['privileged_protected_attributes'],
                                                 favorable_label=self.hyperparams['favorable_label'], 
                                                 unfavorable_label=unfavorable_label)
 
@@ -137,17 +167,17 @@ class FairnessPreProcessing(TransformerPrimitiveBase[Inputs, Outputs, Hyperparam
         if self.hyperparams['algorithm'] == 'Disparate_Impact_Remover':
             transformed_dataset = algorithms.preprocessing.DisparateImpactRemover().fit_transform(ibm_dataset)
         elif self.hyperparams['algorithm'] == 'Learning_Fair_Representations':
-            unprivileged_protected_attributes = list(set(inputs[protected_attributes[0]].unique()) - set(self.hyperparams['privileged_protected_attributes']))
+            unprivileged_protected_attributes = list(set(self.inputs[protected_attributes[0]].unique()) - set(self.hyperparams['privileged_protected_attributes']))
             transformed_dataset = algorithms.preprocessing.LFR(unprivileged_groups = ({protected_attributes[0]: self.hyperparams['privileged_protected_attributes']}),
                                                                 privileged_groups = ({protected_attributes[0]: unprivileged_protected_attributes})).fit_transform(ibm_dataset)
         elif self.hyperparams['algorithm'] == 'Optimized_Preprocessing':
-            unprivileged_protected_attributes = list(set(inputs[protected_attributes[0]].unique()) - set(self.hyperparams['privileged_protected_attributes']))
+            unprivileged_protected_attributes = list(set(self.inputs[protected_attributes[0]].unique()) - set(self.hyperparams['privileged_protected_attributes']))
             transformed_dataset = algorithms.preprocessing.OptimPreproc(unprivileged_groups = ({protected_attributes[0]: self.hyperparams['privileged_protected_attributes']}),
                                                                 privileged_groups = ({protected_attributes[0]: unprivileged_protected_attributes}),
                                                                 optimizer = algorithms.preprocessing.optim_preproc_helpers.opt_tools.OptTools,
                                                                 optim_options = {}).fit_transform(ibm_dataset)
         else: 
-            unprivileged_protected_attributes = list([set(inputs[p_attr].unique()) - set(p_attr_val) \
+            unprivileged_protected_attributes = list([set(self.inputs[p_attr].unique()) - set(p_attr_val) \
                                 for p_attr, p_attr_val in zip(protected_attributes, self.hyperparams['privileged_protected_attributes'])])
             privileged_groups = {p_attr: p_attr_val for (p_attr, p_attr_val) in zip(protected_attributes, self.hyperparams['privileged_protected_attributes'])}
             unprivileged_groups = {p_attr: p_attr_val for (p_attr, p_attr_val) in zip(protected_attributes, unprivileged_protected_attributes)}
@@ -156,5 +186,18 @@ class FairnessPreProcessing(TransformerPrimitiveBase[Inputs, Outputs, Hyperparam
         # transform IBM dataset back to D3M dataset
         return CallResult(transformed_dataset.convert_to_dataframe()[0])
 
-if __name__ == '__main__':
+    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+        """
+        Parameters
+        ----------
+        inputs : D3M dataframe
+
+        Returns
+        ----------
+        Outputs : exact same D3M dataframe. Pre-processing algorithm not applied to test dataset
+            
+        """
+
+        # pass dataframe through unchanged
+        return CallResult(inputs)
 
