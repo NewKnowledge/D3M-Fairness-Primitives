af6e2bc702206b6577174e0fccd5bddabe5e8511
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-03 11:10:34 -0500

    update module names in init

--- FairnessPrimitives/__init__.py
+++ FairnessPrimitives/__init__.py
@@ -1,6 +1,6 @@
-from FairnessPrimitives.FairnessPreProcessing import FairnessPreProcessing
-#from FairnessPrimitives.FairnessInProcessing import FairnessInProcessing
-#from FairnessPrimitives.FairnessPostProcessing import FairnessPostProcessing
+from FairnessPrimitives.pre_processing import FairnessPreProcessing
+#from FairnessPrimitives.in_processing import FairnessInProcessing
+#from FairnessPrimitives.post_processing import FairnessPostProcessing
 
 __version__ = '1.0.0'
 
