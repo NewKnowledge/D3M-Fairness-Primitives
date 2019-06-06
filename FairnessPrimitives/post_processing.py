8996bbf50b5b03f295a6fab69038bb2f28dd325a
jgleason <jeffrey.gleason@newknowledge.io>
2019-06-06 16:01:07 -0500

    add v1 post processing prim and pipeline

---
+++ FairnessPrimitives/post_processing.py
@@ -0,0 +1,202 @@
+import sys
+import os.path
+import pandas
+from typing import List
+
+from d3m.primitive_interfaces.base import CallResult, PrimitiveBase
+
+from d3m import container, utils, exceptions
+from d3m.container import DataFrame as d3m_DataFrame
+from d3m.metadata import hyperparams, base as metadata_base, params
+from common_primitives import random_forest
+
+from aif360 import datasets, algorithms
+from aif360.algorithms import postprocessing
+
+__author__ = 'Distil'
+__version__ = '1.0.0'
+__contact__ = 'mailto:nklabs@newknowledge.com'
+
+Inputs = container.pandas.DataFrame
+Outputs = container.pandas.DataFrame
+
+class Params(params.Params):
+    pass
+
+class Hyperparams(hyperparams.Hyperparams):
+    algorithm = hyperparams.Enumeration(default = 'Calibrated_Equality_of_Odds', 
+        semantic_types = ['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        values = ['Calibrated_Equality_of_Odds', 'Equality_of_Odds', 'Reject_Option_Classification'],
+        description = 'type of fairness pre-processing algorithm to use')
+    protected_attribute_cols = hyperparams.List(
+        elements=hyperparams.Hyperparameter[int](-1),
+        default=[],
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+        description="A set of column indices to use as protected attributes.",
+    )
+    favorable_label = hyperparams.Bounded[float](
+        lower=0.,
+        upper=1., 
+        default=1.,
+        description='label value which is considered favorable (i.e. positive) in the binary label case',
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/ControlParameter'],
+    )
+    pass
+
+class FairnessPostProcessing(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
+    '''
+        Primitive that applies one of three post-processing algorithm after a learning algorithm has been fit to change 
+        the output labels according to some fairness objective or criteria. Algorithm options are 'Calibrated_Equality_of_Odds', 
+        'Equality_of_Odds', and 'Reject_Option_Classification'.
+    '''
+    metadata = metadata_base.PrimitiveMetadata({
+        # Simply an UUID generated once and fixed forever. Generated using "uuid.uuid4()".
+        'id': "f29f6404-fbe5-4017-bb72-5c47efc5a415",
+        'version': __version__,
+        'name': "Post-processing Fairness Techniques",
+        # Keywords do not have a controlled vocabulary. Authors can put here whatever they find suitable.
+        'keywords': ['fairness, bias, debias, data postprocessing, data augmentation'],
+        'source': {
+            'name': __author__,
+            'contact': __contact__,
+            'uris': [
+                # Unstructured URIs.
+                "https://github.com/NewKnowledge/D3M-Fairness-Primitives",
+            ],
+        },
+        # A list of dependencies in order. These can be Python packages, system packages, or Docker images.
+        # Of course Python packages can also have their own dependencies, but sometimes it is necessary to
+        # install a Python package first to be even able to run setup.py of another package. Or you have
+        # a dependency which is not on PyPi.
+         'installation': [
+             {
+            'type': metadata_base.PrimitiveInstallationType.PIP,
+            'package_uri': 'git+https://github.com/NewKnowledge/D3M-Fairness-Primitives.git@{git_commit}#egg=FairnessPrimitives'.format(
+                git_commit=utils.current_git_commit(os.path.dirname(__file__)),
+             ),
+        }],
+        # The same path the primitive is registered with entry points in setup.py.
+        'python_path': 'd3m.primitives.data_augmentation.data_conversion.FairnessPostProcessing',
+        # Choose these from a controlled vocabulary in the schema. If anything is missing which would
+        # best describe the primitive, make a merge request.
+        'algorithm_types': [
+            metadata_base.PrimitiveAlgorithmType.DATA_CONVERSION,
+        ],
+        'primitive_family': metadata_base.PrimitiveFamily.DATA_AUGMENTATION,
+    })
+
+    def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0)-> None:
+        super().__init__(hyperparams=hyperparams, random_seed=random_seed)
+
+        self.attributes = None
+        self.targets = None
+        self.values = None
+        self.clf = None
+
+    def get_params(self) -> Params:
+        return self._params
+
+    def set_params(self, *, params:Params) -> None:
+        self.params = params
+
+    def set_training_data(self, *, inputs: Inputs, outputs: Outputs) -> None:
+        '''
+        Sets primitive's training data 
+
+        Parameters
+        ----------
+        inputs : dataset containing predicted labels
+        outputs : dataset containing true labels
+        '''
+
+        print(list(inputs), file = sys.__stdout__)
+        print(list(out), file = sys.__stdout__)
+                                                
+        # only select attributes from training data
+        self.targets = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/TrueTarget')
+        if not len(self.targets):
+            self.targets = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/TrueTarget')
+        if not len(self.targets):
+            self.targets = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/SuggestedTarget')
+        label_names = [list(inputs)[t] for t in self.targets]
+        
+        # calculate protected attributes and privel=
+        protected_attributes = [list(inputs)[c] for c in self.hyperparams['protected_attribute_cols']]
+
+        # save index and metadata
+        idx = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/PrimaryKey')
+        idx = [list(inputs)[i] for i in idx]
+        index = inputs[idx]
+        
+        # mark attributes that are not priveleged data
+        attributes = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/Attribute')
+        priveleged_data = inputs.metadata.get_columns_with_semantic_type('https://metadata.datadrivendiscovery.org/types/PrivilegedData')
+        self.attributes = list(set(attributes) - set(priveleged_data))
+        attribute_names = [list(inputs)[a] for a in self.attributes]
+        
+        # drop index from training data
+        inputs = inputs.drop(columns=idx)
+
+        # transfrom dataframe to IBM 360 compliant dataset
+            # 1. assume datacleaning primitive has been applied so there are no NAs
+            # 2. assume categorical columns have been converted to unique numeric values
+            # 3. assume the label column is numeric 
+        unfavorable_label = 0. if self.hyperparams['favorable_label'] == 1. else 1.
+        ibm_dataset = datasets.BinaryLabelDataset(df = inputs,
+                                                label_names = label_names,
+                                                protected_attribute_names = protected_attributes,
+                                                favorable_label=self.hyperparams['favorable_label'],
+                                                unfavorable_label=unfavorable_label)
+
+        # apply pre-processing algorithm
+        if self.hyperparams['algorithm'] == 'Disparate_Impact_Remover':
+            transformed_dataset = preprocessing.DisparateImpactRemover().fit_transform(ibm_dataset)
+        elif self.hyperparams['algorithm'] == 'Learning_Fair_Representations':
+            transformed_dataset = preprocessing.LFR(unprivileged_groups = [{protected_attributes[0]: ibm_dataset.unprivileged_protected_attributes}],
+                                                                privileged_groups = [{protected_attributes[0]: ibm_dataset.privileged_protected_attributes}]).fit_transform(ibm_dataset)
+        else: 
+            privileged_groups = [{p_attr: p_attr_val} for (p_attr, p_attr_val) in zip(protected_attributes, ibm_dataset.privileged_protected_attributes)]
+            unprivileged_groups = [{p_attr: p_attr_val} for (p_attr, p_attr_val) in zip(protected_attributes, ibm_dataset.unprivileged_protected_attributes)]
+            transformed_dataset = preprocessing.Reweighing(unprivileged_groups = unprivileged_groups, privileged_groups = privileged_groups).fit_transform(ibm_dataset)
+            # TODO: incorporate instance weights fro transformed_dataset.instance_weights into classifier
+
+        # transform IBM dataset back to D3M dataset
+        df = transformed_dataset.convert_to_dataframe()[0].drop(columns = label_names)
+        df = d3m_DataFrame(pandas.concat([index.reset_index(drop=True), inputs[label_names].reset_index(drop = True), df.reset_index(drop=True)], axis = 1))
+        df.metadata = inputs.metadata
+        self.values = df
+
+    def fit(self, *, timeout: float = None, iterations: int = None) -> CallResult[None]:
+        """
+        Fit primitive using sklearn random forest on pre-processed training data
+
+        Parameters
+        ----------
+        inputs : D3M dataframe
+
+        Returns
+        ----------
+        Outputs : D3M dataframe unchanged
+        """
+        
+        hp_class = random_forest.RandomForestClassifierPrimitive.metadata.query()['primitive_code']['class_type_arguments']['Hyperparams'] 
+        hp=hp_class.defaults().replace({'use_inputs_columns': self.attributes, 'use_outputs_columns': self.targets})
+        self.clf = random_forest.RandomForestClassifierPrimitive(hyperparams=hp)
+        self.clf.set_training_data(inputs = self.values, outputs = self.values)
+        return self.clf.fit()
+
+    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+        """
+        Produce predictions using sklearn random forest 
+
+        Parameters
+        ----------
+        inputs : D3M dataframe
+
+        Returns
+        ----------
+        Outputs : predictions from sklearn random forest which was fit on pre-processed training data
+            
+        """
+        return self.clf.produce(inputs = inputs)
+
---
+++ FairnessPrimitives/post_processing_pipeline_uu5_heartstatlog.py
@@ -0,0 +1,72 @@
+from d3m import index
+from d3m.metadata.base import ArgumentType, Context
+from d3m.metadata.pipeline import Pipeline, PrimitiveStep
+
+# Creating pipeline
+pipeline_description = Pipeline()
+pipeline_description.add_input(name='inputs')
+
+# Step 0: Denormalize primitive
+step_0 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.denormalize.Common'))
+step_0.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='inputs.0')
+step_0.add_output('produce')
+pipeline_description.add_step(step_0)
+
+# Step 1: dataset_to_dataframe
+step_1 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.dataset_to_dataframe.Common'))
+step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
+step_1.add_hyperparameter(name='dataframe_resource', argument_type= ArgumentType.VALUE, data='learningData')
+step_1.add_output('produce')
+pipeline_description.add_step(step_1)
+
+# Step 2: DISTIL/NK data cleaning
+step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.data_cleaning.Datacleaning'))
+step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_2.add_output('produce')
+pipeline_description.add_step(step_2)
+
+# Step 3: column_parser
+step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.DataFrameCommon'))
+step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+step_3.add_output('produce')
+pipeline_description.add_step(step_3)
+
+# Step 4: imputer
+step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
+step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+step_4.add_output('produce')
+step_4.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
+step_4.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
+pipeline_description.add_step(step_4)
+
+# Step 6: Random Forest classifier
+step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.classification.random_forest.DataFrameCommon'))
+step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+step_5.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+step_5.add_output('produce')
+step_5.add_hyperparameter(name='use_inputs_columns', argument_type=ArgumentType.VALUE,data=[2, 3, 4, 5, 6, 7])
+step_5.add_hyperparameter(name='use_outputs_columns', argument_type=ArgumentType.VALUE,data=[1])
+pipeline_description.add_step(step_5)
+
+# Step 5: Fairness post-processing Distil
+step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_preprocessing.data_conversion.FairnessPreProcessing'))
+step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+step_6.add_output('produce')
+step_6.add_hyperparameter(name='algorithm', argument_type=ArgumentType.VALUE,data='Calibrated_Equality_of_Odds')
+step_6.add_hyperparameter(name='protected_attribute_cols', argument_type=ArgumentType.VALUE,data=[3])
+step_6.add_hyperparameter(name='favorable_label', argument_type=ArgumentType.VALUE,data=0.)
+pipeline_description.add_step(step_6)
+
+# Step 7: construct output
+step_7 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.DataFrameCommon'))
+step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
+step_7.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_7.add_output('produce')
+pipeline_description.add_step(step_7)
+
+# Final Output
+pipeline_description.add_output(name='output predictions', data_reference='steps.7.produce')
+
+# Output to JSON
+with open('pipeline.json', 'w') as outfile:
+    outfile.write(pipeline_description.to_json())
