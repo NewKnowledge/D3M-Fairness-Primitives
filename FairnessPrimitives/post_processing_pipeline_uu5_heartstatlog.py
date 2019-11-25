a352bee91637798a132c3ad429fcec4d7ef27c4a
David Sullivan <davesullivan41@gmail.com>
2019-11-25 12:46:51 -0600

    Modify post pipeline to be nearly identical to pre pipeline

--- FairnessPrimitives/post_processing_pipeline_uu5_heartstatlog.py
+++ FairnessPrimitives/post_processing_pipeline_uu5_heartstatlog.py
@@ -19,30 +19,37 @@ step_1.add_hyperparameter(name='dataframe_resource', argument_type= ArgumentType
 step_1.add_output('produce')
 pipeline_description.add_step(step_1)
 
+#d3m.primitives.data_transformation.remove_columns.Common
+step_n = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.remove_columns.Common'))
+step_n.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_n.add_hyperparameter(name='columns', argument_type=ArgumentType.VALUE,data=[8, 9, 10, 11, 12, 13, 14])
+step_n.add_output('produce')
+pipeline_description.add_step(step_n)
+
 # Step 2: DISTIL/NK data cleaning
 step_2 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.data_cleaning.Datacleaning'))
-step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
 step_2.add_output('produce')
 pipeline_description.add_step(step_2)
 
 # Step 3: column_parser
 step_3 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.column_parser.Common'))
-step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
 step_3.add_output('produce')
 pipeline_description.add_step(step_3)
 
-## Step 4: imputer
-#step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
-#step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-#step_4.add_output('produce')
-#step_4.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
-#step_4.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
-#pipeline_description.add_step(step_4)
+# Step 4: imputer
+step_4 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
+step_4.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+step_4.add_output('produce')
+step_4.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
+step_4.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
+pipeline_description.add_step(step_4)
 
 # Step 5: Random Forest classifier
 step_5 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.classification.random_forest.Common'))
-step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
-step_5.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.3.produce')
+step_5.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
+step_5.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.5.produce')
 step_5.add_output('produce')
 step_5.add_hyperparameter(name='use_inputs_columns', argument_type=ArgumentType.VALUE,data=[2, 3, 4, 5, 6, 7])
 step_5.add_hyperparameter(name='use_outputs_columns', argument_type=ArgumentType.VALUE,data=[1])
@@ -50,17 +57,24 @@ pipeline_description.add_step(step_5)
 
 # Step 6: Fairness post-processing Distil
 step_6 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_augmentation.data_conversion.FairnessPostProcessing'))
-step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
-step_6.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.4.produce')
+step_6.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
+step_6.add_argument(name='outputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.6.produce')
 step_6.add_output('produce')
 step_6.add_hyperparameter(name='algorithm', argument_type=ArgumentType.VALUE,data='Calibrated_Equality_of_Odds')
 step_6.add_hyperparameter(name='protected_attribute_cols', argument_type=ArgumentType.VALUE,data=[3])
 step_6.add_hyperparameter(name='favorable_label', argument_type=ArgumentType.VALUE,data=0.)
 pipeline_description.add_step(step_6)
 
+# Step 7: construct output
+step_7 = PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_transformation.construct_predictions.Common'))
+step_7.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.7.produce')
+step_7.add_argument(name='reference', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+step_7.add_output('produce')
+pipeline_description.add_step(step_7)
+
 # Final Output
-pipeline_description.add_output(name='output predictions', data_reference='steps.5.produce')
+pipeline_description.add_output(name='output predictions', data_reference='steps.8.produce')
 
 # Output to JSON
 with open('pipeline.json', 'w') as outfile:
-   outfile.write(pipeline_description.to_json())
+    outfile.write(pipeline_description.to_json())
